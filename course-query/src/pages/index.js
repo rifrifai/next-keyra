51400000b100d3b9a9a098c8af4fce49f5cf5873
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-11-02 11:33:22 +0700

    QueryClient, QueryClientProvider

--- course-query/src/pages/index.js
+++ course-query/src/pages/index.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import localFont from "next/font/local";
 import axios from "axios";
+import { list } from "postcss";
+import { useQuery } from "@tanstack/react-query";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,17 +16,31 @@ const geistMono = localFont({
 });
 
 export default function Home() {
-  const [pokemon, setPokemon] = useState([]);
+  // const [pokemon, setPokemon] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon")
-      .then((result) => setPokemon(result.data.results));
-  }, []);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   axios.get("https://pokeapi.co/api/v2/pokemon").then((result) => {
+  //     setPokemon(result.data.results);
+  //     setIsLoading(false);
+  //   });
+  // }, []);
+
+  const { data, isFetching } = useQuery({
+    queryKey: ["pokemon-list"],
+    queryFn: () => axios.get("https://pokeapi.co/api/v2/pokemon"),
+  });
 
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    ></div>
+    >
+      {isFetching ? (
+        <p>Loading...</p>
+      ) : (
+        data?.data.results.map((item) => <li key={item.name}>{item.name}</li>)
+      )}
+    </div>
   );
 }
